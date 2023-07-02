20087eff6d49b2b726c7d2c17aad2e5a0bc8f8f0
Yousef Amar <yousefamar@gmail.com>
2023-07-02 12:47:29 +0000

    add: speech bubbles and cards to inspect cells

--- static/world/app.vue
+++ static/world/app.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="max-w-2xl mx-auto p-8">
-    <article class="prose max-w-2xl">
-      <h1>Mini World</h1>
+    <article class="max-w-2xl">
+      <div class="prose">
+        <h1>Miniverse</h1>
 
-      <p>This is an experiment of a small world with autonomous agents powered by LLMs.</p>
+        <p>This is an experiment of a small 32x32 world with autonomous emoji agents powered by LLMs. See <a href="https://yousefamar.com/memo/projects/miniverse/" target="_blank">the project page</a> for more details.</p>
 
-      <div class="world flex flex-col">
-        <div class="flex flex-row" v-for="(row, y) in world?.cells" :key="y">
-          <div class="cell" v-for="(cell, x) in row" :key="x"><span v-if="cell === ' '">&nbsp;</span><span v-else v-html="cell?.string"></span></div>
+        <p>Click <code>Tick</code> to step through the simulation, and click a cell for more info on it.</p>
+
+        <div class="flex flex-row mb-2 justify-center">
+          <button class="btn btn-primary" @click="tick">Tick</button>
         </div>
       </div>
 
-      <div class="flex flex-row">
-        <button class="btn btn-primary" @click="tick">Tick</button>
+      <div class="world flex flex-col">
+        <div class="flex flex-row" v-for="(row, y) in world?.cells" :key="y">
+          <div class="cell" :class="[ cell && 'occupied', cell?.metadata?.speechCurr && 'speaking' ]" v-for="(cell, x) in row" :key="x" @click="selectCell(cell)"><span v-if="cell === ' '">&nbsp;</span><span v-else v-html="cell?.string"></span></div>
+        </div>
+      </div>
+
+      <div v-if="selectedCell" class="card w-full bg-neutral shadow-xl mt-2">
+        <div class="card-body">
+          <h2 class="card-title">{{ selectedCell.string }} {{ selectedCell.label }} <div class="badge">{{ selectedCell.className }}</div></h2>
+          <p>Current position: ({{ selectedCell.x }}, {{ selectedCell.y }})</p>
+          <p v-if="selectedCell.metadata?.speechCurr"><b>Saying:</b> {{ selectedCell.metadata.speechCurr }}</p>
+          <p v-if="selectedCell.metadata?.speechPrec"><b>Previously said:</b> {{ selectedCell.metadata.speechPrev }}</p>
+        </div>
       </div>
     </article>
 
@@ -23,9 +36,14 @@ export default {
   data() {
     return {
       world: null,
+      selectedCell: null,
     }
   },
   methods: {
+    selectCell(cell) {
+      this.selectedCell = cell;
+    },
+
     async tick() {
       const res = await fetch('/world/tick');
       const data = await res.json();
@@ -55,12 +73,34 @@ export default {
 <style scoped>
 .world {
   font-size: min(1em, 2vw);
+  align-items: center;
 }
 
 .cell {
+  position: relative;
   text-align: center;
   line-height: 1.1em;
   width: 1.1em;
   height: 1.1em;
+}
+
+.cell.occupied {
+  cursor: pointer;
+}
+
+@keyframes speaking {
+  0% { content: ""; }
+  50% { content: "💬"; }
+  100% { content: ""; }
+}
+
+.speaking span::before {
+  pointer-events: none;
+  position: absolute;
+  bottom: 15%;
+  left: 40%;
+  opacity: 80%;
+  content: "";
+  animation: speaking 1s infinite;
 }
 </style>
