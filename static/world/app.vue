fe4a0ad632d20e295120df4c98c50735d9d9e415
Yousef Amar <yousefamar@gmail.com>
2023-07-02 13:13:28 +0000

    add: UI for loading states

--- static/world/app.vue
+++ static/world/app.vue
@@ -6,29 +6,30 @@
 
         <p>This is an experiment of a small 32x32 world with autonomous emoji agents powered by LLMs. See <a href="https://yousefamar.com/memo/projects/miniverse/" target="_blank">the project page</a> for more details.</p>
 
-        <p>Click <code>Tick</code> to step through the simulation, and click a cell for more info on it.</p>
+        <p>Click <code>Tick</code> to step through the simulation, and click a cell for more info on it. Ticks may take a while.</p>
 
         <div class="flex flex-row mb-2 justify-center">
-          <button class="btn btn-primary" @click="tick">Tick</button>
-        </div>
+          <button class="btn btn-primary" :disabled="tickLoading" @click="tick"><span v-show="tickLoading" class="loading loading-spinner"></span> Tick</button></div>
       </div>
 
-      <div class="world flex flex-col">
-        <div class="flex flex-row" v-for="(row, y) in world?.cells" :key="y">
+      <div v-if="world" class="world flex flex-col">
+        <div class="flex flex-row" v-for="(row, y) in world.cells" :key="y">
           <div class="cell" :class="[ cell && 'occupied', cell?.metadata?.speechCurr && 'speaking' ]" v-for="(cell, x) in row" :key="x" @click="selectCell(cell)"><span v-if="cell === ' '">&nbsp;</span><span v-else v-html="cell?.string"></span></div>
         </div>
+      </div>
+      <div v-else class="flex flex-row justify-center">
+        <span class="loading loading-ring loading-lg my-8"></span>
       </div>
 
       <div v-if="selectedCell" class="card w-full bg-neutral shadow-xl mt-2">
         <div class="card-body">
           <h2 class="card-title">{{ selectedCell.string }} {{ selectedCell.label }} <div class="badge">{{ selectedCell.className }}</div></h2>
-          <p>Current position: ({{ selectedCell.x }}, {{ selectedCell.y }})</p>
+          <p><b>Current position:</b> ({{ selectedCell.x }}, {{ selectedCell.y }})</p>
           <p v-if="selectedCell.metadata?.speechCurr"><b>Saying:</b> {{ selectedCell.metadata.speechCurr }}</p>
           <p v-if="selectedCell.metadata?.speechPrec"><b>Previously said:</b> {{ selectedCell.metadata.speechPrev }}</p>
         </div>
       </div>
     </article>
-
   </div>
 </template>
 <script>
@@ -37,6 +38,7 @@ export default {
     return {
       world: null,
       selectedCell: null,
+      tickLoading: false,
     }
   },
   methods: {
@@ -45,16 +47,23 @@ export default {
     },
 
     async tick() {
-      const res = await fetch('/world/tick');
-      const data = await res.json();
-      this.world = data;
+      this.tickLoading = true;
+      try {
+        const res = await fetch('/world/tick');
+        const data = await res.json();
+        this.world = data;
 
-      for (const row of this.world.cells) {
-        for (const cell of row) {
-          if (cell?.className === 'NPC' && cell.metadata?.speechCurr) {
-            console.log(cell.label, 'said', cell.metadata.speechCurr);
+        for (const row of this.world.cells) {
+          for (const cell of row) {
+            if (cell?.className === 'NPC' && cell.metadata?.speechCurr) {
+              console.log(cell.label, 'said', cell.metadata.speechCurr);
+            }
           }
         }
+      } catch (error) {
+        alert(error);
+      } finally {
+        this.tickLoading = false;
       }
     },
   },
